e9a134247620362fb6d44767d0709bf51f972978
DongKi Lee <ki7348@naver.com>
2021-09-30 16:47:31 +0900

    boj17129 boj14940_ki7348.python

---
+++ ki7348/dfs&bfs/boj17129.py
@@ -0,0 +1,54 @@
+import sys
+from collections import deque
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+result = 0
+
+def bfs(x,y,visited,result):
+    queue = deque()
+    queue.append((x,y))
+    graph[x][y] = 0
+    visited[x][y] = True
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if nx < 0 or ny < 0 or nx>=n or ny>=m:
+                continue
+            if visited[nx][ny] == False and (graph[nx][ny] == 3 or graph[nx][ny] == 4 or graph[nx][ny] == 5):
+                result = graph[x][y] + 1
+                return result
+            if graph[nx][ny] != 1 and visited[nx][ny] == False:
+                visited[nx][ny] = True
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+    return 0
+
+n, m = map(int,sys.stdin.readline().split())
+
+graph = []
+visited = [[False]*m for _ in range(n)]
+
+for _ in range(n):
+    graph.append(list(map(int,sys.stdin.readline().strip())))
+
+flag = True
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 2:
+            k = (bfs(i,j,visited,result))
+            flag = False
+            break
+    if flag == False:
+        break
+
+if k:
+    print("TAK")
+    print(k)
+else:
+    print("NIE")
