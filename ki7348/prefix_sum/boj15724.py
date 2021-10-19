bd4fb53ec19adb8ac4ab36ca7e2a2bb8ac1eade7
unknown <ki7348@uos.ac.kr>
2021-10-19 23:19:08 +0900

    boj11660 boj15724 boj16507_ki7348.python

---
+++ ki7348/prefix_sum/boj15724.py
@@ -0,0 +1,17 @@
+import sys
+
+n,m=map(int,sys.stdin.readline().split())
+
+arr = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+
+prefix_sum = [[0]*(m+1) for _ in range(n+1)]
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        prefix_sum[i][j] = arr[i-1][j-1] + prefix_sum[i][j-1] + prefix_sum[i-1][j] - prefix_sum[i-1][j-1]
+
+case = int(sys.stdin.readline())
+
+for i in range(case):
+    x1,y1,x2,y2 = map(int,sys.stdin.readline().split())
+    print(prefix_sum[x2][y2]-prefix_sum[x1-1][y2]-prefix_sum[x2][y1-1]+prefix_sum[x1-1][y1-1])
